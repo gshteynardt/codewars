94a66d22e9914b515694a0090d120ac18ce49c79
GermanShtey <gvshteynardt@gmail.com>
2021-02-13 21:39:58 +0300

    added kata money

---
+++ 7-kyu/money_money.js
@@ -0,0 +1,35 @@
+//Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+//
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+//
+// Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+//
+// Example:
+//
+//   Let P be the Principal = 1000.00
+//   Let I be the Interest Rate = 0.05
+//   Let T be the Tax Rate = 0.18
+//   Let D be the Desired Sum = 1100.00
+//
+//
+// After 1st Year -->
+//   P = 1041.00
+// After 2nd Year -->
+//   P = 1083.86
+// After 3rd Year -->
+//   P = 1128.30
+// Thus Mr. Scrooge has to wait for 3 years for the initial principal to amount to the desired sum.
+//
+// Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+
+const calculateYears = (principal, interest, tax, desired)  => {
+  let years = 0;
+  while(principal < desired){
+    principal += (principal * interest) * (1 - tax);
+    years++;
+  }
+  return years;
+}
+
+calculateYears(1000, 0.05, 0.18, 1100); //3
+calculateYears(1000,0.01625,0.18,1200); //14
